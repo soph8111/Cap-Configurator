6be54fa268c707359ef13a6110b26a00463ac608
Sophie Spang <soph8111@stud.kea.dk>
2021-10-05 19:51:40 +0200

    animation works

--- script.js
+++ script.js
@@ -28,33 +28,80 @@ function toggleOption(event) {
   }
 
   // If feature is (now) turned on:
-  // - mark target as chosen (add class "chosen")
-  // - un-hide the feature-layer(s) in the #product-preview;
-  // - create featureElement and append to #selected ul
+  // - mark target as chosen (add class "chosen") ✓
+  // - un-hide the feature-layer(s) in the #product-preview; ✓
+  // - create featureElement and append to #selected ul ✓
   // - create FLIP-animation to animate featureElement from img in target, to
   //   its intended position. Do it with normal animation or transition class!
 
   // Else - if the feature (became) turned off:
-  // - no longer mark target as chosen
-  // - hide the feature-layer(s) in the #product-preview
-  // - find the existing featureElement in #selected ul
+  // - no longer mark target as chosen ✓
+  // - hide the feature-layer(s) in the #product-preview ✓
+  // - find the existing featureElement in #selected ul ✓
   // - create FLIP-animation to animate featureElement to img in target
   // - when animation is complete, remove featureElement from the DOM
 
   if (features[feature]) {
     // feature added
     console.log(`Feature ${feature} is turned on!`);
+
+    // TODO: More code
     target.classList.add("chosen");
     document.querySelector(`[data-feature=${feature}]`).classList.remove("hide");
 
-    // TODO: More code
+    const featureElement = createFeatureElement(feature);
+    document.querySelector("#selected ul").append(featureElement);
+
+    // Flip animation part:
+    const clickFrame = target.getBoundingClientRect();
+    const addedFrame = featureElement.getBoundingClientRect();
+
+    const deltaX = clickFrame.left - addedFrame.left;
+    const deltaY = clickFrame.top - addedFrame.top;
+
+    featureElement.animate(
+      [
+        { transformOrigin: "top left", transform: `translateX(${deltaX}px) translateY(${deltaY}px)` },
+        { transformOrigin: "top left", transform: "none" },
+      ],
+      {
+        duration: 500,
+        easing: "ease-in-out",
+      }
+    );
   } else {
     // feature removed
     console.log(`Feature ${feature} is turned off!`);
+
+    // TODO: More code
     target.classList.remove("chosen");
     document.querySelector(`[data-feature=${feature}]`).classList.add("hide");
 
-    // TODO: More code
+    // Flip animation part:
+    const featureElement = document.querySelector(`#selected [data-feature=${feature}]`);
+    console.log(featureElement);
+
+    const clickFrame = target.getBoundingClientRect();
+    const addedFrame = featureElement.getBoundingClientRect();
+
+    const deltaX = clickFrame.left - addedFrame.left;
+    const deltaY = clickFrame.top - addedFrame.top;
+
+    const animation = [
+      { transformOrigin: "top left", transform: `none` },
+      { transformOrigin: "top left", transform: `translateX(${deltaX}px) translateY(${deltaY}px)` },
+    ];
+
+    const properties = {
+      duration: 500,
+      easing: "ease-in-out",
+    };
+
+    const animationEnd = featureElement.animate(animation, properties);
+
+    animationEnd.onfinish = () => {
+      document.querySelector(`#selected [data-feature=${feature}]`).remove();
+    };
   }
 }
 
